e7f40cef6011313ba434519a422fdf5d712d16f6
duwalshiva <iamshivaduwal@gmail.com>
2025-08-10 18:07:06 +0545

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Projects\\bus_fare_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Flutter\\flutter"
+  "PROJECT_DIR=C:\\Projects\\bus_fare_app"
+  "FLUTTER_ROOT=C:\\Flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Projects\\bus_fare_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Projects\\bus_fare_app"
+  "FLUTTER_TARGET=C:\\Projects\\bus_fare_app\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzIuMw==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049NWMxNDMzNTA5Zg==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049MzFjNDg3NWM3YQ==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My44LjE="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Projects\\bus_fare_app\\.dart_tool\\package_config.json"
+)
